3039aa20e3cc7b3cf54ff3fe426798172a7f8722
Edward <k3s47@outlook.com>
2018-10-27 08:30:26 +0700

    2nd
    push

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,6 +31,42 @@ $(function () {
         $('select.form-control').removeClass('form-control').css('width', '100%');
     }
 });
-// Main
-console.log('FullStack Vietnam!');
+$(function () {
+    console.log("start !");
+});
+// $("ul li p").hide();
+// $("ul li h2").on("click",function(){
+// 	var $arr = $(this).parent();
+//     var $this = $(this).parent().find('p');
+//     $("li p").not($this).hide();
+//     $("li").not($arr).removeClass("arrow-down")
+//     // here is what I want to do
+//     $this.toggle();
+//     $arr.toggleClass("arrow-down");
+// });
+
+
+/*
+
+$("item").each
+
+
+*/
+
+$("ul li p").hide();
+$("ul li h2").on("click", function () {
+    var $this = $(this).parent().find("p");
+    var $arr = $(this).parent();
+    $("ul li").not($arr).removeClass("arrow-down");
+    $("li p").not($this).hide();
+    $this.toggle(5000);
+    $arr.toggleClass("arrow-down");
+});
+
+setTimeout(function () {
+    console.log(1);
+});
+setInterval(function () {
+    console.log(2);
+});
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,39 @@
-// Main
-console.log('FullStack Vietnam!')
+$(function(){
+	console.log("start !")
+} )
+// $("ul li p").hide();
+// $("ul li h2").on("click",function(){
+// 	var $arr = $(this).parent();
+//     var $this = $(this).parent().find('p');
+//     $("li p").not($this).hide();
+//     $("li").not($arr).removeClass("arrow-down")
+//     // here is what I want to do
+//     $this.toggle();
+//     $arr.toggleClass("arrow-down");
+// });
+
+
+/*
+
+$("item").each
+
+
+*/
+
+$("ul li p").hide();
+$("ul li h2").on("click",function(){
+	var $this = $(this).parent().find("p");
+	var $arr = $(this).parent();
+	$("ul li").not($arr).removeClass("arrow-down");
+	$("li p").not($this).hide();
+	$this.toggle(5000);
+	$arr.toggleClass("arrow-down");
+
+})
+
+setTimeout(function(){
+	console.log(1);
+})
+setInterval(function(){
+	console.log(2);
+})
